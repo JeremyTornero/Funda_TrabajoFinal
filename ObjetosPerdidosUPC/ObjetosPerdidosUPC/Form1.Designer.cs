eee930845d0856d1393659e3f1769e9ab804f53e
Jeremy Tornero <auaforever@gmail.com>
2016-02-23 04:45:25 -0500

    FirstForm

--- ObjetosPerdidosUPC/ObjetosPerdidosUPC/Form1.Designer.cs
+++ ObjetosPerdidosUPC/ObjetosPerdidosUPC/Form1.Designer.cs
@@ -34,20 +34,22 @@
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(23, 12);
+            this.button2.Font = new System.Drawing.Font("Calibri", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.Location = new System.Drawing.Point(54, 46);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(111, 131);
+            this.button2.Size = new System.Drawing.Size(206, 46);
             this.button2.TabIndex = 0;
             this.button2.Text = "Administrativo";
             this.button2.UseVisualStyleBackColor = true;
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(181, 12);
+            this.button1.Font = new System.Drawing.Font("Calibri", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(54, 113);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(111, 131);
+            this.button1.Size = new System.Drawing.Size(206, 46);
             this.button1.TabIndex = 0;
-            this.button1.Text = "Administrativo";
+            this.button1.Text = "Liderman";
             this.button1.UseVisualStyleBackColor = true;
             // 
             // Form1
@@ -56,7 +58,7 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackgroundImage = global::ObjetosPerdidosUPC.Properties.Resources.Background;
             this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
-            this.ClientSize = new System.Drawing.Size(494, 340);
+            this.ClientSize = new System.Drawing.Size(318, 208);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.button2);
             this.Name = "Form1";
